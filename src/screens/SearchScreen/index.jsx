d7a67832f7dafdf6bef921f6c641b5bd1e3cada8
hkung77 <henrykung@live.ca>
2019-12-01 20:48:28 +0900

    include cors in request header

--- src/screens/SearchScreen/index.jsx
+++ src/screens/SearchScreen/index.jsx
@@ -39,7 +39,8 @@ const SearchScreen = () => {
     await fetch(URL, {
       method: "POST",
       headers: {
-        "Content-Type": "application/json"
+        "Content-Type": "application/json",
+        "Access-Control-Allow-Origin": "*"
       },
       body: JSON.stringify({ search_term: searchTerm })
     })
